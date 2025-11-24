5a3cf726cfeb0c3e5cfd9d02ec9af055d3452d03
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-11-24 17:49:35 +0300

    cleanup a little bit...

--- internal/cards/rune.go
+++
@@ -1,5 +0,0 @@
-package card
-
-type RuneCardData struct {
-	ImgPath string `json:"ImgPath"`
-}
--- internal/cards/spell.go
+++
@@ -1,9 +0,0 @@
-package card
-
-type SpellCardData struct {
-	Name           string `json:"Name"`
-	Description    string `json:"Description"`
-	DecorationText string `json:"DecorationText"`
-	ImgPath        string `json:"ImgPath"`
-	Power          int    `json:"Power"`
-}
